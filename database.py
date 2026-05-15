a90387dcb3d0ebd39a14f42aae44d5f397434f1c
Coder-yt <animeupdatesau04@gmail.com>
2026-05-15 21:52:24 +0530

    database.py

--- database.py
+++ database.py
@@ -13,70 +13,68 @@ from config import MONGO_URI
 import time
 
 client = AsyncIOMotorClient(MONGO_URI)
+
 db = client.rename_bot
 
 users = db.users
 
 #-------------------------#
 
-async def get_premium_status(uid):
-user = await users.find_one({"_id": uid})
-if not user:
-return False
-
-if not user.get("premium"):  
-    return False  
-
-expiry = user.get("premium_expiry", 0)  
-
-if expiry and time.time() > expiry:  
-    await users.update_one(  
-        {"_id": uid},  
-        {"$set": {"premium": False, "premium_expiry": 0}}  
-    )  
-    return False  
-
-return True
-
-#-------------------------#
-
 async def get_user(uid):
-return await users.find_one({"_id": uid})
+    return await users.find_one({"_id": uid})
+
 
 async def set_user(uid, data):
-await users.update_one(
-{"_id": uid},
-{"$set": data},
-upsert=True
-)
+    await users.update_one(
+        {"_id": uid},
+        {"$set": data},
+        upsert=True
+    )
 
 #-------------------------#
 
 async def add_user(uid):
-await users.update_one(
-{"_id": uid},
-{
-"$setOnInsert": {
-"prefix": "",
-"suffix": "",
-"caption": "",
-"thumb": "",
-"banned": False
-}
-},
-upsert=True
-)
+    await users.update_one(
+        {"_id": uid},
+        {
+            "$setOnInsert": {
+                "prefix": "",
+                "suffix": "",
+                "caption": "",
+                "thumb": "",
+                "banned": False,
+                "premium": False
+            }
+        },
+        upsert=True
+    )
 
 #-------------------------#
 
 async def is_banned(uid):
-user = await get_user(uid)
-return user.get("banned", False) if user else False
+    user = await get_user(uid)
+
+    return user.get("banned", False) if user else False
 
 #-------------------------#
 
 async def get_all_users():
-return await users.find({}).to_list(length=None)
+    return await users.find({}).to_list(length=None)
+
+#-------------------------#
+
+async def get_premium_status(uid):
+    user = await get_user(uid)
+
+    if not user:
+        return False
+
+    return user.get("premium", False)
+
+#-------------------------#
+
+async def setup_database():
+    print("Database Connected ✅")
 
 #-------------------------#
 
