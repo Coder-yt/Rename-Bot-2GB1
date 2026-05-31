aa7bff53919f085f7873b6ad33c9c40d49b290e3
MD-Developer-yt <animeworldday2005@gmail.com>
2026-05-31 14:15:32 +0530

    database.py

--- database.py
+++ database.py
@@ -8,17 +8,61 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URI
 import time
 
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
 client = AsyncIOMotorClient(MONGO_URI)
 
 db = client.rename_bot
 
 users = db.users
 
-#-------------------------#
+leaderboard = db.leaderboard
+
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
+async def update_leaderboard(user_id):
+
+    await leaderboard.update_one(
+        {"user_id": user_id},
+        {
+            "$inc": {
+                "today": 1,
+                "weekly": 1,
+                "monthly": 1,
+                "alltime": 1
+            },
+            "$set": {
+                "user_id": user_id
+            }
+        },
+        upsert=True
+    )
+
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
 
 async def get_user(uid):
     return await users.find_one({"_id": uid})
 
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
 
 async def set_user(uid, data):
     await users.update_one(
@@ -27,7 +71,12 @@ async def set_user(uid, data):
         upsert=True
     )
 
-#-------------------------#
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
 
 async def add_user(uid):
     await users.update_one(
@@ -45,19 +94,31 @@ async def add_user(uid):
         upsert=True
     )
 
-#-------------------------#
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
 
 async def is_banned(uid):
     user = await get_user(uid)
 
     return user.get("banned", False) if user else False
 
-#-------------------------#
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
 
 async def get_all_users():
     return await users.find({}).to_list(length=None)
 
-#-------------------------#
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
 
 async def get_premium_status(uid):
     user = await get_user(uid)
@@ -67,7 +128,11 @@ async def get_premium_status(uid):
 
     return user.get("premium", False)
 
-#-------------------------#
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
 
 async def setup_database():
     print("Dᴀᴛᴀʙᴀsᴇ Cᴏɴɴᴇᴄᴛᴇᴅ ✅")
